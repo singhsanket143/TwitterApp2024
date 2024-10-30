4592f40f9e0fcfda4a2053ad47a5377298ea26dd
Sanket Singh <singhsanket143@gmail.com>
2024-10-30 23:10:15 +0530

    Added not found

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.get('/ping', (req, res) => {
     });
 }); // what to do if someone makes a GET request to /ping
 
-app.post('/hello', (req, res) => {
+app.post('/hello/*', (req, res) => {
     console.log("query params", req.query); // query params
     console.log("req body", req.body); // req body
     return res.json({
@@ -28,6 +28,12 @@ app.get('/tweets/:tweet_id/comments/:comment_id', (req, res) => {
     console.log(req.params); // url params
     return res.json({
         message: 'tweet details'
+    });
+});
+
+app.all('*', (req, res) => {
+    return res.status(404).json({
+        message: 'Not found'
     });
 });
 
