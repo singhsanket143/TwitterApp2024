faf84e80463fdb8cdcba9f7ef57dad920feba245
Sanket Singh <singhsanket143@gmail.com>
2024-11-03 13:25:55 +0530

    Added zod for validation

--- src/routes/v1/tweet.js
+++ src/routes/v1/tweet.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { createTweet, getTweetById, getTweets } from '../../controllers/tweetController.js';
+import { validate } from '../../validators/zodValidator.js';
+import { tweetZodSchema } from '../../validators/tweetZodSchema.js';
 
 const router = express.Router(); // Create a new router object
 
@@ -8,7 +10,7 @@ router.get('/', getTweets);
 
 router.get('/:id', getTweetById);
 
-router.post('/', createTweet);
+router.post('/', validate(tweetZodSchema), createTweet);
 
 export default router; // Export the router object
 
---
+++ src/validators/zodValidator.js
@@ -0,0 +1,16 @@
+export const validate = (schema) => {
+    // it returns a validating middleware
+    return async (req, res, next) => {
+        try {
+            console.log(req.body);
+            schema.parse(req.body);
+            next();
+        } catch(error) {
+            return res.status(400).json({
+                error: error.errors,
+                success: false,
+                message: "Validation failed"
+            });
+        }
+    }
+}
